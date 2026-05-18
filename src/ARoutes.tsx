12026c5de7af8b8288c10a9a621bb5e150c19424
Terminal-Adawe <bede.abbe91@gmail.com>
2026-05-18 12:21:28 +0000

    changes

--- src/ARoutes.tsx
+++ src/ARoutes.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Routes, Navigate, BrowserRouter as Router } from "react-router-dom";
-import App from "./App.tsx";
 import { ApplicationProvider } from "../resources/providers/ApplicationProvider.tsx";
 import { useTokenExpiry } from './hooks/useAuth';
 import Menu from "./pages/loggedIn/Menu.tsx";
@@ -17,13 +16,13 @@ const ARoutes: React.FC = () => {
     return <ApplicationProvider>
               <Router>
                 <Routes>
-                    <Route path='/' element={<App />} />
+                    {/* <Route path='/' element={<App />} /> */}
                     <Route path='/about' element={<AboutPage />} />
                     <Route path='/login' element={<LoginPage />} />
                     <Route path='/signup' element={<SignUpPage />} />
                     {/* <Route path='/learn/*' element={<ProtectedRoute><Menu /></ProtectedRoute>} /> */}
                     <Route path='/admin/*' element={<Menu />} />
-                    <Route path="*" element={<Navigate replace to="/" />} />
+                    <Route path="*" element={<Navigate replace to="/admin/home" />} />
                 </Routes>
               </Router>
             </ApplicationProvider>
